7c7f71cd8f1776ee832667d3f549090fffc02409
root <admin@ldc.int>
2019-04-27 17:58:34 +0800

    working

--- executor/destroy.sh
+++ executor/destroy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Execute terraform destroy
-cd /home/master/Projects/terravSphere
-terraform destroy -var-file=~/Projects/terravSphere/Swarm/swarm.tfvars -var-file=~/Projects/terravSphere/DR.tfvars -state=~/Projects/terravSphere/Swarm/swarm.tfstate
+cd /home/master/Projects/automate-vSphere
+terraform destroy -var-file=~/Projects/automate-vSphere/createVMs/k8s/k8s.tfvars -var-file=~/Projects/automate-vSphere/createVMs/DR.tfvars -state=~/Projects/automate-vSphere/createVMs/k8s/k8s.tfstate
