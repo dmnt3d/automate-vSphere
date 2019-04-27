2c308a7ab87bc74914e341447e1070821c0f0517
root <admin@ldc.int>
2019-04-27 19:08:56 +0800

    setup

---
+++ executor/setupMasterAndJoin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Execute terraform destroy
+cd /home/master/automate-vSphere/setupVMs
+
+# setup ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+ansible-playbook -i ../vm-inventory --private-key=/home/master/.ssh/id_rsa prep-for-master.yml
+ansible-playbook -i ../vm-inventory --private-key=/home/master/.ssh/id_rsa prep-for-worker.yml
