206d26805d42e90a0d7468fa0adc203bef8de3bb
root <admin@ldc.int>
2019-04-27 21:58:27 +0800

    test

--- executor/setupMasterAndJoin.sh
+++ executor/setupMasterAndJoin.sh
@@ -3,6 +3,7 @@
 cd /home/master/automate-vSphere/setupVMs
 
 # setup ssh-agent
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 ansible-playbook -i ../vm-inventory --private-key=/home/master/.ssh/id_rsa prep-for-master.yml
